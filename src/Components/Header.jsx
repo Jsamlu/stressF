0ce4a9295f29a609ec0ec813aa9e5809a7f396e1
Jsamlu <jayeshsamlu69@gmail.com>
2024-09-07 03:18:46 +0530

    hw

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header (){
   return (
-    <div>
+    <div className="h-full w-full">
       <div className="flex flex-col bg-red-500">
         <a href="/">Home</a>
         <a href="/test">test</a>
