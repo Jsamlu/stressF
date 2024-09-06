884fbf6e811b27b941fc3b42d9f6f3bf550790c6
Jsamlu <jayeshsamlu69@gmail.com>
2024-09-07 03:05:05 +0530

    test

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Header (){
   return (
-    <>
-      <p>header</p>
-    </>
+    <div>
+      <div className="flex flex-col bg-red-500">
+        <a href="/">Home</a>
+        <a href="/test">test</a>
+      </div>
+    </div>
   );
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,33 @@
 import Hero from "./Hero.jsx"
+import React, { useState, useEffect } from 'react';
 
-// import Header from '../Header.jsx';
+import Header from '../Header.jsx';
 // import Footer from '../Footer.jsx'
 
 export default function Home() {
+  const [showHeader, setShowHeader] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollThreshold = window.innerHeight * 0.8; // 80% of the viewport height
+      setShowHeader(window.scrollY > scrollThreshold);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+    }, []);
+
   return (
     <>
     <Hero/>
+    <div
+        className={`md:flex hidden fixed top-0 left-0 w-full z-50 transition-all duration-700 ease-in-out transform ${showHeader ? 'opacity-100 translate-y-0' : 'opacity-0 -translate-y-full'
+          }`}
+      >
+        <Header />
+    </div>
     <div className="m-10 lg:mr-[300px] lg:ml-[300px] grid lg:grid gap-10">
       <div className="mt-10 grid grid-cols-2  gap-5  items-center rounded-xl shadow-[0_35px_60px_-15px_rgba(0,0,0,0.3)] ">
         <div className="rounded-tl-xl rounded-bl-xl h-full lg:h-[300px] object-cover overflow-hidden">
