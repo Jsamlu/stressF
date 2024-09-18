e14ccaac9d3f2515103b95cc827320f21121743a
Jsamlu <jayeshsamlu69@gmail.com>
2024-09-18 17:47:57 +0530

    forum

--- src/Components/Test/Forum.jsx
+++ src/Components/Test/Forum.jsx
@@ -7,50 +7,79 @@ export default function Forum() {
   return (
     <div className="min-h-screen bg-indigo-200 ">
       <div className="md:hidden"><S_header/></div>
-      <div className="p-10 pl-[100px] pr-[100px]">
+        <div className="p-10 md:pt-3  pt-[80px]">
+        <div className=" bg-white my-10 w-[95%] mx-auto px-[15%] py-[5%] rounded-xl">
+            <h1 className="md:text-2xl text-lg font-bold text-blue-500">How the test works:</h1>
+            <ul className="mt-5 list-decimal flex flex-col gap-4 text-gray-500  lg:text-xl md:2xl  text-lg">
+              <li>
+                <p className="">
+
+              You’ll answer questions based on your daily activities and how
+              you’re feeling.
+                </p>
+              </li>
+              <li>
+              <p>
+
+               For each question, you’ll have 5 answer choices
+              ranging from "Strongly Disagree" to "Strongly Agree."
+              </p>
+              </li>
+              <li>
+              <p>
+
+               Based on your answers, we’ll assess your stress levels and recommend some
+              activities tailored just for you!
+              </p>
+              </li>
+            </ul>
+          </div>
+          </div>
+      <div className=" px-[15%] py-[5%]">
+        
         {QList.map((QList) => {
           return (
             <div className="mb-5 p-10 bg-white rounded-xl">
-              <form action="">
-              <p >
+              <form>
+              <p className="md:text-2xl text-sm text-blue-500">
                 {QList.id}.&nbsp; 
                 {QList.question}
               </p>
-              <div className="pl-10 mt-5">
-                <ul className="">
-                  <li className="flex  mt-3 p-3 bg-purple-100 rounded-xl">
-                    <input className="w-[15px] h-[15px] mr-5" type="radio" name="ans" value="1"/>
-                    <span> 
+              <div className="p-4 mt-5">
+                <ul className="flex flex-col gap-y-3">
+                  <li className="flex p-3 bg-sky-200 rounded-xl">
+                    <input className="w-[20px] h-[20px] self-center mr-5" type="radio" name="ans" value="1"/>
+                    <p className="md:text-lg text-xs"> 
                         {QList.an1}
-                    </span>
+                    </p>
                   </li>
 
-                  <li className="flex  mt-3 p-3 bg-purple-100 rounded-xl">
-                    <input className="w-[15px] h-[15px] mr-5" type="radio" name="ans" value="1" ></input>
-                    <span> 
+                  <li className="flex p-3 bg-sky-200 rounded-xl">
+                    <input className="w-[20px] h-[20px] self-center mr-5" type="radio" name="ans" value="1" ></input>
+                    <p className="md:text-lg text-xs"> 
                         {QList.an2}
-                    </span>
+                    </p>
                   </li>
 
-                  <li className="flex  mt-3 p-3 bg-purple-100 rounded-xl">
-                    <input className="w-[15px] h-[15px] mr-5" type="radio" name="ans" value="1" ></input>
-                    <span> 
+                  <li className="flex p-3 bg-sky-200 rounded-xl">
+                    <input className="w-[20px] h-[20px] self-center mr-5" type="radio" name="ans" value="1" ></input>
+                    <p className="md:text-lg text-xs">  
                         {QList.an3}
-                    </span>
+                    </p>
                   </li>
 
-                  <li className="flex  mt-3 p-3 bg-purple-100 rounded-xl">
-                    <input className="w-[15px] h-[15px] mr-5" type="radio" name="ans" value="1" ></input>
-                    <span> 
+                  <li className="flex p-3 bg-sky-200 rounded-xl">
+                    <input className="w-[20px] h-[20px] self-center mr-5" type="radio" name="ans" value="1" ></input>
+                    <p className="md:text-lg text-xs"> 
                         {QList.an4}
-                    </span>
+                    </p>
                   </li>
 
-                  <li className="flex  mt-3 p-3 bg-purple-100 rounded-xl">
-                    <input className="w-[15px] h-[15px] mr-5" type="radio" name="ans" value="1" ></input>
-                    <span> 
+                  <li className="flex p-3 bg-sky-200 rounded-xl">
+                    <input className="w-[20px] h-[20px] self-center mr-5" type="radio" name="ans" value="1" ></input>
+                    <p className="md:text-lg text-xs">  
                         {QList.an5}
-                    </span>
+                    </p>
                   </li>
 
                 </ul>
