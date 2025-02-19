3cdae627d5bdd3a558952abaa81e0c0dd2b8e667
Jsamlu <jayeshsamlu69@gmail.com>
2025-02-19 11:28:16 +0530

    jay changes

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import Login from "../Login/Login";
 import { Dashboard } from ".";
 
-// import { useContext } from "react";
-// import { login_context } from "../Context/login/login_context";
-
 function Profile() {
-  // const { setUser } = useContext(login_context);
-  // console.log(setUser);
+ 
 
   return <div className="h-[70vh]">{ true ? <Dashboard /> : <Login />}</div>;
 }
