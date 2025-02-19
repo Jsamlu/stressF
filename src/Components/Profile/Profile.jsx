b9d497175651348cb6a12eab390ddacc4cd6ecc2
Jsamlu <jayeshsamlu69@gmail.com>
2025-02-19 11:19:05 +0530

    update before front and back merge

--- src/Components/Profile/Profile.jsx
+++ src/Components/Profile/Profile.jsx
@@ -2,14 +2,12 @@ import React from "react";
 import Login from "../Login/Login";
 import { Dashboard } from ".";
 
-import { useContext } from "react";
-import { login_context } from "../Context/login/login_context";
+
 
 function Profile() {
-  const { setUser } = useContext(login_context);
-  console.log(setUser);
 
-  return <div className="h-[70vh]">{ true ? <Dashboard /> : <Login />}</div>;
+
+  return <div className="h-[70vh]">{ false ? <Dashboard /> : <Login />}</div>;
 }
 
 export default Profile;
